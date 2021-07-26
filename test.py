6f3ae523033a8ea63837bbc71ad485d4ca1ee3e0
Pratheek S <pratheekseraje@gmail.com>
2021-07-26 20:25:30 +0530

    Blueticks Test.

--- test.py
+++ test.py
@@ -1,16 +1,30 @@
-def alternatingCharacters(s):
+def alternatingRanges(s, up):
     count = 0
-    for i in range(len(s) - 1):
-        if s[i] == s[i + 1]:
-            count += 1
-    return count
+    str_list = []
+    for i in range(len(s)):
+        try:
+            if s[i] == s[i+1] - 1:
+                continue
+            else:
+                if (s[i] + 1) == (s[i+1] - 1):
+                    str_list.append(str(s[i] + 1))
+                else:
+                    if s[i + 1] == upper:
+                        s[i + 1] = upper + 1
+                    str_list.append(str(s[i] + 1) + '->' + str(s[i+1] - 1))
+        except IndexError:
+            pass
+    return str_list
 
 
 if __name__ == '__main__':
-    q = int(input().strip())
-
-    for q_itr in range(q):
-        s = input()
-
-        result = alternatingCharacters(s)
-        print(result)
+    lower = 0
+    upper = 99
+    q = [0, 1, 3, 50, 75]
+    if lower not in q:
+        q.insert(0, lower)
+    if upper not in q:
+        q.append(99)
+    q = sorted(q)
+    result = alternatingRanges(q, upper)
+    print(result)
